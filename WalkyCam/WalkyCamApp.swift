56af0e1f3b28ceaadeacc5c6968696b0bff5dd41
Gabriel <gabrielrgrocha14@gmail.com>
2023-10-10 16:29:04 -0300

    Initial Commit

---
+++ WalkyCam/WalkyCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkyCamApp.swift
+//  WalkyCam
+//
+//  Created by Gabriel Gazal on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WalkyCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
