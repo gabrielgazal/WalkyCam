dfacdc14d4a926a2414c44139f1513b645670f80
Gabriel <gabrielrgrocha14@gmail.com>
2024-09-15 03:30:39 -0300

    Added a few more scenes for watch application

--- WalkyCam/WalkyCamWatch Watch App/Feature/WatchMenu/WatchMenuView.swift
+++ WalkyCam/WalkyCamWatch Watch App/Feature/WatchMenu/WatchMenuView.swift
@@ -9,52 +9,105 @@ struct WatchMenuView: View {
         self.viewModel = viewModel
     }
     
-    // MARK: - View Body
+    @State private var navigateToNextView = false
+    @State private var isAnimating = false
+    @State private var scale: CGFloat = 1.0 // Estado para controlar o scale effect
     
+    // MARK: - View Body
     var body: some View {
-        ZStack {
-            Color.negro
-                .ignoresSafeArea()
-            VStack(
-                alignment: .center,
-                spacing: Tokens.Size.Spacing.tiny) {
-                    HStack {
-                        Text("Hi \(viewModel.getUserName())")
-                            .font(.projectFont(size: Tokens.Size.Font.regular))
-                        Spacer()
-                        Image(systemName: "person")
-                            .frame(
-                                width: Tokens.Size.Spacing.regular,
-                                height: Tokens.Size.Spacing.regular
-                            )
-                    }
-                    Asset.logo.swiftUIImage
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(Color.naranja)
-                    Image("StreetCam")
-                        .resizable()
-                        .scaledToFit()
-                        .environment(\.colorScheme, .dark)
-                        .layoutPriority(1)
-                        .frame(height: 70)
-                    HStack {
-                        Spacer()
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(Color.naranja)
-                            .frame(width: 27, height: 27)
-                            .onTapGesture {
-                                print("TESTE")
+        NavigationView {
+            ZStack {
+                Color.negro
+                    .ignoresSafeArea()
+                
+                VStack(
+                    alignment: .center,
+                    spacing: Tokens.Size.Spacing.tiny) {
+                        
+                        HStack {
+                            Text("Hi \(viewModel.getUserName())")
+                                .font(.projectFont(size: Tokens.Size.Font.regular))
+                            Spacer()
+                            Image(systemName: "person")
+                                .frame(
+                                    width: Tokens.Size.Spacing.regular,
+                                    height: Tokens.Size.Spacing.regular
+                                )
+                        }
+                        .isHidden(isAnimating)
+                        
+                        Asset.logo.swiftUIImage
+                            .renderingMode(.template)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color.naranja)
+                            .isHidden(isAnimating)
+                        
+                        VStack(alignment: .center, spacing: 0) {
+                            Image("StreetCam")
+                                .resizable()
+                                .scaledToFit()
+                                .environment(\.colorScheme, .dark)
+                                .layoutPriority(1)
+                                .frame(height: 70)
+                                .scaleEffect(scale) // Aplica o scale effect à imagem
+                            
+                            if isAnimating {
+                                GeometryReader { geometry in
+                                    Path { path in
+                                        path.move(to: CGPoint(x: 0, y: geometry.size.height / 2))
+                                        path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height / 2))
+                                    }
+                                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [2, 2])) // Define o padrão de traço
+                                    .foregroundColor(Color.grisOscuro)
+                                }
+                                .frame(height: 1)
                             }
-                            .overlay {
-                                Image(systemName: "chevron.right")
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.naranja)
+                                .frame(width: 27, height: 27)
+                                .overlay {
+                                    Image(systemName: "chevron.right")
+                                        .renderingMode(.template)
+                                        .foregroundColor(.negro)
+                                }
+                                .isHidden(isAnimating)
+                                .onTapGesture {
+                                    withAnimation(.easeInOut(duration: 1)) {
+                                        isAnimating = true
+                                    }
+                                    withAnimation(.easeInOut(duration:  1.5)) {
+                                        scale = 0
+                                    }
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                        navigateToNextView = true
+                                    }
+                                }
+                            
+                            NavigationLink(destination: WatchSearchView(), isActive: $navigateToNextView) {
+                                EmptyView()
                             }
+                            .buttonStyle(PlainButtonStyle())
+                        }
                     }
-                }
-                .padding(.vertical, Tokens.Size.Spacing.tiny)
-                .padding(.horizontal, Tokens.Size.Spacing.small)
+                    .padding(.vertical, Tokens.Size.Spacing.tiny)
+                    .padding(.horizontal, Tokens.Size.Spacing.small)
+            }
         }
+    }
+}
+
+struct NextView: View {
+    var body: some View {
+        Text("Nova Tela")
+            .font(.largeTitle)
+            .navigationTitle("Nova Tela")
     }
 }
 
